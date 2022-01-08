0494748fc6e0315d85419e78fcd038d37f1198a4
Andy <officialandylee@gmail.com>
2022-01-08 15:34:38 +0800

    Initial Commit

---
+++ Shared/laneCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  laneCheckApp.swift
+//  Shared
+//
+//  Created by Andy Lee on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct laneCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
